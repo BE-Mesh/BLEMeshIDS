5f27261a5e01dd5ba582a5044578f52f81cb0aba
Andrea Lacava <thecave003@gmail.com>
2020-08-25 12:27:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def clean_df(df):
+    del df["experiment"]
+    del df["setting"]
+    del df["run"]
+    del df["source node"]
+    del df["node"]
+
+
+def nodes_connected(G, u, v):
+    return u in G.neighbors(v)
+
+
+def save_cleaned_copy(df):
+    clean_df(df)
+    df.to_csv("experiment_I.csv", encoding='utf-8')
+
+
+if __name__ == '__main__':
+    src_path = "A:\\Download\\Tesi\\Dataset BLE MESH\\experiment_I_rpi.csv"
+    sub_path = "A:\\Download\\Tesi\\Dataset BLE MESH\\sub.csv"
+    path = "experiment_I.csv"
+
+    df = pd.read_csv(path)
+    # df = pd.read_csv(src_path , sep=', ', engine='python')
+
+    # print(df.keys())
+    # print(set(list(df["src"]) + (list(df["dest"]))))
+    #
+    # nodes = list(set(list(df["src"]) + (list(df["dest"]))))
+
+    # print(nodes)
+
+    # save_cleaned_copy(df)
+
+    df = df.drop(df[df.dest == "fffd"].index)  # broadcast
+    df = df.drop(df[df.src == "0000"].index)  # ??
+    df = df.drop(df[df.dest == "0000"].index)  # ??
+
+    G: nx.DiGraph = nx.from_pandas_edgelist(df, 'src', 'dest', create_using=nx.DiGraph)
+    print(len(G.nodes()))
+    print(len(G.edges()))
+
+    # Plot it
+    # nx.draw(G, with_labels=True))
+
+    nx.draw_networkx(G, arrows=True)
+
+    # print(nodes_connected(G, "010e", "0102"))
+
+    # pos = nx.spring_layout(G)
+    # nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'))
+    # nx.draw_networkx_labels(G, pos)
+    # nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='r', arrows=True)
+
+    plt.show()
