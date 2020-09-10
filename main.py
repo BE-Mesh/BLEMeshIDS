f84767edbd5ccd3e1dfd15fcfbdf5e937095e406
Andrea Lacava <thecave003@gmail.com>
2020-09-10 19:17:41 +0200

    preprocessor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
 
 
 def clean_df(dataframe):
@@ -42,17 +44,50 @@ def plot_dataset(dataframe):
 
 
 if __name__ == '__main__':
-    common_path = "A:\\Download\\Tesi\\Dataset BLE MESH\\"  # "/home/thecave3/Scaricati/btmesh-dataset/"
-    src_path = common_path + "experiment_I_rpi.csv"
+    common_path = "/home/thecave3/Scaricati/btmesh-dataset/"  # "A:\\Download\\Tesi\\Dataset BLE MESH\\"
+    src_path = common_path + "experiment_II_lpn.csv"
     sub_path = common_path + "sub.csv"
     path = "data/experiment_I.csv"
 
-    df = pd.read_csv(path)
-    # df = pd.read_csv(src_path, sep=', ', engine='python')
-    # clean_df(df)
-    # df.to_csv(path)
+    # df = pd.read_csv(path)
+    df = pd.read_csv(src_path, sep=', ', engine='python')
 
-    # print(set(list(df["src"]) + (list(df["dest"]))))
-    # nodes = list(set(list(df["src"]) + (list(df["dest"]))))
+    # df.sample(n=500).to_csv(path)
 
-    # plot_dataset(df)
+    t_window = 1000000  # ms
+    min_time = df["sent time"].min()
+    df["time_window"] = np.floor((df["sent time"] - min_time) / t_window)
+    dfs = []
+    for i in range(int(df["time_window"].max())):
+        window = df.loc[df["time_window"] == i]
+        if not window.empty:
+            temp_dict = {"size": window.size, "rssi_mean": np.mean(window["rssi"]), "rssi_std": np.std(window["rssi"]),
+                         "ttl_mean": np.mean(window["ttl"]), "ttl_std": np.std(window["ttl"])}
+            unique, counts = np.unique(window["src"], return_counts=True)
+            pkts_src = list(dict(zip(unique, counts)).values())  # packtes per source
+            temp_dict["src_mean"] = np.mean(pkts_src)
+            temp_dict["src_std"] = np.std(pkts_src)
+            unique, counts = np.unique(window["dest"], return_counts=True)
+            pkts_dst = list(dict(zip(unique, counts)).values())  # packtes per dest
+            temp_dict["dest_mean"] = np.mean(pkts_dst)
+            temp_dict["dest_std"] = np.std(pkts_dst)
+            # tmp_dict[""]
+            # TODO finish features extraction
+            # print(temp_dict)
+            dfs.append(pd.DataFrame(temp_dict, index=[0]))
+
+    result_df = pd.concat(dfs)
+
+    result_np = result_df.to_numpy()
+    pca = PCA(2)  # target dimension
+    projected = pca.fit_transform(result_np)
+    plt.scatter(projected[:, 0], projected[:, 1])
+    plt.xlabel('component 1')
+    plt.ylabel('component 2')
+    plt.title("PCA of Dataset from experiment II")
+    plt.colorbar()
+    plt.show()
+
+# print(df["sent time"])
+
+# df["sent time"] = (df["sent time"] - min_time)
