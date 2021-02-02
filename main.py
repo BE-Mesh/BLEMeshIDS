42132e1a62db7028d415ccb8b0b93c9b55989417
EmanueleGiacomini <emanuele.giacomo@gmail.com>
2021-02-02 16:33:28 +0100

    cleaned repo

--- main.py
+++ main.py
@@ -26,16 +26,14 @@ def trainer_routine():
 WINDOW_LEN = 10  # s
 
 if __name__ == '__main__':
-    logging.info(f'Executing training with window length: {WINDOW_LEN} seconds')
+    #logging.info(f'Executing training with window length: {WINDOW_LEN} seconds')
     #generator.generate_dataset('data/raw', 'data/proc', window_size=int(WINDOW_LEN * 1e6))
     #trainer.train()
-    plotter.plot(window_size=WINDOW_LEN)
-    """for i in range(1, 10, 2):
-        print(f'Starting window size of {i} seconds')
+    #plotter.plot(window_size=WINDOW_LEN)
+    for i in range(1, 10, 2):
+        logging.info(f'Executing training with window length: {WINDOW_LEN} seconds')
         #splitter.split()
         generator.generate_dataset('data/raw', 'data/proc', window_size=int(i * 1e6))
         trainer.train()
         plotter.plot(window_size=i)
-        print(f'Done with window size of {i} seconds')
-
-    print('Finished')"""
+        logging.info(f'Completed training.')
